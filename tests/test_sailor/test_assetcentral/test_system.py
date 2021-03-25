55d3b7acf028d7533008119796540eabb9c5f57e
Jaakob Kind <jaakob.kind@sap.com>
2021-03-25 13:27:24 +0100

    commit before pull

--- tests/test_sailor/test_assetcentral/test_system.py
+++ tests/test_sailor/test_assetcentral/test_system.py
@@ -11,14 +11,14 @@ def test_find_functions_expect_fetch_call_args():
     find_params = dict(extended_filters=['integer_param1 < 10'], string_parameter=['Type A', 'Type F'])
     expected_call_args = (['integer_param1 lt 10'], [["string_parameter eq 'Type A'", "string_parameter eq 'Type F'"]])
 
-    with patch.object(System, '_prepare_components'):
-        objects = [System({'systemId': x}) for x in ['test_id1', 'test_id2']]
-        expected_result = SystemSet(objects)
-        with patch('sailor.assetcentral.system._ac_application_url') as mock:
-            mock.return_value = 'base_url'
-            with patch('sailor.assetcentral.system._fetch_data') as mock_fetch:
-                mock_fetch.return_value = [{'systemId': 'test_id1'}, {'systemId': 'test_id2'}]
-                actual_result = find_systems(**find_params)
+    # with patch.object(System, '_prepare_components'):
+    objects = [System({'systemId': x}) for x in ['test_id1', 'test_id2']]
+    expected_result = SystemSet(objects)
+    with patch('sailor.assetcentral.system._ac_application_url') as mock:
+        mock.return_value = 'base_url'
+        with patch('sailor.assetcentral.system._fetch_data') as mock_fetch:
+            mock_fetch.return_value = [{'systemId': 'test_id1'}, {'systemId': 'test_id2'}]
+            actual_result = find_systems(**find_params)
 
     assert constants.VIEW_SYSTEMS in mock_fetch.call_args.args[0]
     assert mock_fetch.call_args.args[1:] == expected_call_args
