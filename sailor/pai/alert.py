eed1e0911f32f1579afb852bf8e1110855f969ab
Atreju Tauschinsky <atreju.florian.tauschinsky@sap.com>
2021-08-05 16:04:19 +0200

    migrate Alert to use new field structure (#61)
    * migrate Alert to use new field structure

--- sailor/pai/alert.py
+++ sailor/pai/alert.py
@@ -7,83 +7,69 @@ Classes are provided for individual Alert as well as groups of Alerts (AlertSet)
 
 from .constants import ALERTS_READ_PATH
 from .utils import PredictiveAssetInsightsEntity, _pai_application_url
-from ..assetcentral.utils import _fetch_data, _add_properties, _parse_filter_parameters, ResultSet
-from ..utils.timestamps import _odata_to_timestamp_parser
+from ..assetcentral.utils import (_fetch_data, _add_properties_new, _parse_filter_parameters,
+                                  ResultSet, _AssetcentralField)
+from ..utils.timestamps import _odata_to_timestamp_parser_new
 
 
-@_add_properties
+_ALERT_FIELDS = [
+    _AssetcentralField('description', 'Description'),
+    _AssetcentralField('severity_code', 'SeverityCode'),
+    _AssetcentralField('category', 'Category'),
+    _AssetcentralField('equipment_name', 'EquipmentName'),
+    _AssetcentralField('model_name', 'ModelName'),
+    _AssetcentralField('indicator_name', 'IndicatorName'),
+    _AssetcentralField('indicator_group_name', 'IndicatorGroupName'),
+    _AssetcentralField('template_name', 'TemplateName'),
+    _AssetcentralField('count', 'Count'),
+    _AssetcentralField('status_code', 'StatusCode'),
+    _AssetcentralField('triggered_on', 'TriggeredOn', get_extractor=_odata_to_timestamp_parser_new()),
+    _AssetcentralField('last_occured_on', 'LastOccuredOn', get_extractor=_odata_to_timestamp_parser_new()),
+    _AssetcentralField('type_description', 'AlertTypeDescription'),
+    _AssetcentralField('error_code_description', 'ErrorCodeDescription'),
+    _AssetcentralField('type', 'AlertType'),
+    _AssetcentralField('id', 'AlertId'),
+    _AssetcentralField('equipment_id', 'EquipmentID'),
+    _AssetcentralField('model_id', 'ModelID'),
+    _AssetcentralField('template_id', 'TemplateID'),
+    _AssetcentralField('indicator_id', 'IndicatorID'),
+    _AssetcentralField('indicator_group_id', 'IndicatorGroupID'),
+    _AssetcentralField('notification_id', 'NotificationId'),
+    _AssetcentralField('_indicator_description', 'IndicatorDescription'),
+    _AssetcentralField('_country_id', 'CountryID'),
+    _AssetcentralField('_functional_location_id', 'FunctionalLocationID'),
+    _AssetcentralField('_maintenance_plant', 'MaintenancePlant'),
+    _AssetcentralField('_functional_location_description', 'FunctionalLocationDescription'),
+    _AssetcentralField('_top_functional_location_name', 'TopFunctionalLocationName'),
+    _AssetcentralField('_planner_group', 'PlannerGroup'),
+    _AssetcentralField('_ref_alert_type_id', 'RefAlertTypeId'),
+    _AssetcentralField('_operator_name', 'OperatorName'),
+    _AssetcentralField('_created_by', 'CreatedBy'),
+    _AssetcentralField('_changed_by', 'ChangedBy'),
+    _AssetcentralField('_serial_number', 'SerialNumber'),
+    _AssetcentralField('_changed_on', 'ChangedOn', get_extractor=_odata_to_timestamp_parser_new()),
+    _AssetcentralField('_processor', 'Processor'),
+    _AssetcentralField('_top_equipment_id', 'TopEquipmentID'),
+    _AssetcentralField('_planning_plant', 'PlanningPlant'),
+    _AssetcentralField('_error_code_id', 'ErrorCodeID'),
+    _AssetcentralField('_operator_id', 'OperatorID'),
+    _AssetcentralField('_source', 'Source'),
+    _AssetcentralField('_top_equipment_name', 'TopEquipmentName'),
+    _AssetcentralField('_created_on', 'CreatedOn', get_extractor=_odata_to_timestamp_parser_new()),
+    _AssetcentralField('_model_description', 'ModelDescription'),
+    _AssetcentralField('_top_equipment_description', 'TopEquipmentDescription'),
+    _AssetcentralField('_functional_location_name', 'FunctionalLocationName'),
+    _AssetcentralField('_top_functional_location_description', 'TopFunctionalLocationDescription'),
+    _AssetcentralField('_top_functional_location_id', 'TopFunctionalLocationID'),
+    _AssetcentralField('_equipment_description', 'EquipmentDescription'),
+]
+
+
+@_add_properties_new
 class Alert(PredictiveAssetInsightsEntity):
     """PredictiveAssetInsights Alert Object."""
 
-    # Properties (in PredictiveAssetInsights terminology) are:
-    # AlertId, AlertType, AlertTypeDescription, Category, ChangedBy, ChangedOn, Count, CountryID,
-    # CreatedBy, CreatedOn, CustomProperty, Description, EquipmentDescription, EquipmentID, EquipmentName,
-    # ErrorCodeDescription, ErrorCodeID, FunctionalLocationID, FunctionalLocationName, FunctionalLocationDescription,
-    # IndicatorDescription, IndicatorGroupID, IndicatorGroupName, IndicatorID, IndicatorName, LastOccuredOn,
-    # MaintenancePlant, ModelDescription, ModelID, ModelName, NotificationId, OperatorID, OperatorName,
-    # lannerGroup, PlanningPlant, Processor, RefAlertTypeId, SerialNumber, SeverityCode, Source, StatusCode,
-    # TemplateID, TemplateName, TopEquipmentDescription, TopEquipmentID, TopEquipmentName, TopFunctionalLocationID,
-    # TopFunctionalLocationName, TopFunctionalLocationDescription, TriggeredOn
-
-    @classmethod
-    def get_available_properties(cls):  # noqa: D102
-        return set(cls._get_legacy_mapping().keys())
-
-    @classmethod
-    def _get_legacy_mapping(cls):
-        """Return a mapping from PredictiveAssetInsights (PAI) terminology to our terminology."""
-        return {
-            'id': ('AlertId', None, None, None),
-            'type': ('AlertType', None, None, None),
-            'type_description': ('AlertTypeDescription', None, None, None),
-            'category': ('Category', None, None, None),
-            'changed_by': ('ChangedBy', None, None, None),
-            'changed_on': ('ChangedOn', _odata_to_timestamp_parser('ChangedOn'), None, None),
-            'count': ('Count', None, None, None),
-            'country_id': ('CountryID', None, None, None),
-            'created_by': ('CreatedBy', None, None, None),
-            'created_on': ('CreatedOn', _odata_to_timestamp_parser('CreatedOn'), None, None),
-            'custom_property': ('CustomProperty', None, None, None),
-            'description': ('Description', None, None, None),
-            'equipment_description': ('EquipmentDescription', None, None, None),
-            'equipment_id': ('EquipmentID', None, None, None),
-            'equipment_name': ('EquipmentName', None, None, None),
-            'error_code_description': ('ErrorCodeDescription', None, None, None),
-            'error_code_id': ('ErrorCodeID', None, None, None),
-            'functional_location_id': ('FunctionalLocationID', None, None, None),
-            'functional_location_name': ('FunctionalLocationName', None, None, None),
-            'functional_location_description': ('FunctionalLocationDescription', None, None, None),
-            'indicator_description': ('IndicatorDescription', None, None, None),
-            'indicator_group_id': ('IndicatorGroupID', None, None, None),
-            'indicator_group_name': ('IndicatorGroupName', None, None, None),
-            'indicator_id': ('IndicatorID', None, None, None),
-            'indicator_name': ('IndicatorName', None, None, None),
-            'last_occured_on': ('LastOccuredOn', _odata_to_timestamp_parser('CreatedOn'), None, None),
-            'maintenance_plant': ('MaintenancePlant', None, None, None),
-            'model_description': ('ModelDescription', None, None, None),
-            'model_id': ('ModelID', None, None, None),
-            'model_name': ('ModelName', None, None, None),
-            'notification_id': ('NotificationId', None, None, None),
-            'operator_id': ('OperatorID', None, None, None),
-            'operator_name': ('OperatorName', None, None, None),
-            'planner_group': ('PlannerGroup', None, None, None),
-            'planning_plant': ('PlanningPlant', None, None, None),
-            'processor': ('Processor', None, None, None),
-            'ref_alert_type_id': ('RefAlertTypeId', None, None, None),
-            'serial_number': ('SerialNumber', None, None, None),
-            'severity_code': ('SeverityCode', None, None, None),
-            'source': ('Source', None, None, None),
-            'template_id': ('TemplateID', None, None, None),
-            'status_code': ('StatusCode', None, None, None),
-            'template_name': ('TemplateName', None, None, None),
-            'top_equipment_description': ('TopEquipmentDescription', None, None, None),
-            'top_equipment_id': ('TopEquipmentID', None, None, None),
-            'top_equipment_name': ('TopEquipmentName', None, None, None),
-            'top_functional_location_id': ('TopFunctionalLocationID', None, None, None),
-            'top_functional_location_name': ('TopFunctionalLocationName', None, None, None),
-            'top_functional_location_description': ('TopFunctionalLocationDescription', None, None, None),
-            'triggered_on': ('TriggeredOn', _odata_to_timestamp_parser('TriggeredOn'), None, None)
-        }
+    _field_map = {field.our_name: field for field in _ALERT_FIELDS}
 
 
 class AlertSet(ResultSet):
