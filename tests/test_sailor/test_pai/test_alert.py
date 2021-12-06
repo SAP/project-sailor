98a62d0e23d52fc5ddd7860f8b93afb5c3339e8e
Oliver Roess <oliver.roess@sap.com>
2021-12-06 16:52:20 +0100

    flake8

--- tests/test_sailor/test_pai/test_alert.py
+++ tests/test_sailor/test_pai/test_alert.py
@@ -84,9 +84,10 @@ class TestAlert():
 
     def test_expected_public_attributes_are_present(self):
         expected_attributes = [
-            'triggered_on', 'last_occured_on', 'count', 'type', 'category', 'severity_code', 'equipment_name', 'model_name', 'indicator_name',
-            'indicator_group_name', 'template_name', 'status_code',  'type_description', 'error_code_description',  'source', 'description',
-            'id', 'equipment_id', 'model_id', 'template_id', 'indicator_id', 'indicator_group_id', 'notification_id', 'error_code_id',
+            'triggered_on', 'last_occured_on', 'count', 'type', 'category', 'severity_code', 'equipment_name',
+            'model_name', 'indicator_name', 'indicator_group_name', 'template_name', 'status_code',  'type_description',
+            'error_code_description',  'source', 'description', 'id', 'equipment_id', 'model_id', 'template_id',
+            'indicator_id', 'indicator_group_id', 'notification_id', 'error_code_id',
         ]
 
         fieldmap_public_attributes = [
