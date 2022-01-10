8b7733d54fd88edd2b71730a38601c59e1df2d3a
Weber <79192034+DisguisedDolphin@users.noreply.github.com>
2022-01-10 17:53:45 +0100

    Fix linting

--- tests/test_sailor/test_utils/test_utils.py
+++ tests/test_sailor/test_utils/test_utils.py
@@ -7,10 +7,13 @@ from sailor.utils.utils import warn_and_log, DataNotFoundWarning
 
 
 @pytest.mark.parametrize('testdescr,input_for_custom_warning_function', [
-    ('Using optional parameter stacklevel', {'message': DataNotFoundWarning(), 'logger_name': __name__, 'stacklevel': 2}),
+    ('Using optional parameter stacklevel',
+     {'message': DataNotFoundWarning(), 'logger_name': __name__, 'stacklevel': 2}),
     ('Only mandatory parameters', {'message': 'Minimal warning message', 'logger_name': __name__}),
-    ('Using all parameters', {'message': 'Warning (all parameters)', 'logger_name': __name__, 'stacklevel': 1, 'category': FutureWarning}),
-    ('Using optional parameter category', {'message': 'Warning message', 'logger_name': __name__, 'category': FutureWarning})
+    ('Using all parameters',
+     {'message': 'Warning (all parameters)', 'logger_name': __name__, 'stacklevel': 1, 'category': FutureWarning}),
+    ('Using optional parameter category',
+     {'message': 'Warning message', 'logger_name': __name__, 'category': FutureWarning})
 ])
 def test_warn_and_log(caplog, input_for_custom_warning_function, testdescr):
     with pytest.warns(None) as record:
