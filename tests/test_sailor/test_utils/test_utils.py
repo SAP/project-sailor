211157ed597be109974f06431d0482888660df75
Weber <79192034+DisguisedDolphin@users.noreply.github.com>
2022-01-14 14:14:31 +0100

    Add test to ensure correct behavior for loggingAdapter regarding stacklevel

--- tests/test_sailor/test_utils/test_utils.py
+++ tests/test_sailor/test_utils/test_utils.py
@@ -27,3 +27,33 @@ def test_log_and_warning_from_adapter(caplog, recwarn, input_for_custom_warning_
     # check that message was logged
     assert len(caplog.records) == 1
     assert caplog.records[-1].message == str(input_for_custom_warning_function['msg'])
+
+
+def call_adapter_directly(logger, msg, stacklevel):
+    logger.warning(msg, stacklevel=stacklevel)
+    logger.error(msg, stacklevel=stacklevel)
+    logger.info(msg, stacklevel=stacklevel)
+    with pytest.warns(UserWarning):
+        logger.log_with_warning(msg)
+
+
+def call_adapter_indirectly(logger, msg, stacklevel):
+    call_adapter_directly(logger, msg, stacklevel)
+
+
+@pytest.mark.parametrize('testdescr,stacklevel,funcName_original,funcName_custom', [
+    ('Call functions on adapter directly', 1, 'call_adapter_directly', 'call_adapter_directly'),
+    ('Call functions on adapter indirectly', 2, 'call_adapter_indirectly', 'call_adapter_directly')
+])
+def test_stacklevel_for_logging_adapter(caplog, stacklevel, funcName_original, funcName_custom, testdescr):
+    logger = logging.getLogger(__name__)
+    logger.addHandler(logging.NullHandler())
+    logger.setLevel(logging.INFO)
+    logger = WarningAdapter(logger)
+
+    call_adapter_indirectly(logger, "Message text", stacklevel)
+
+    assert caplog.records[0].funcName == funcName_original  # when logger.warning is called
+    assert caplog.records[1].funcName == funcName_original  # when logger.error is called
+    assert caplog.records[2].funcName == funcName_original  # when logger.info is called
+    assert caplog.records[3].funcName == funcName_custom    # when logger.log_with_warning is called
