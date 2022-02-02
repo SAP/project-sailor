a4dac393b121facca799f8ddbda43bb4ea493293
Leena Pajo <leena.pajo@sap.com>
2022-02-02 11:01:53 +0100

    new try long message

--- sailor/sap_iot/write.py
+++ sailor/sap_iot/write.py
@@ -79,18 +79,18 @@ def _check_indicator_group_is_complete(uploaded_indicators, group_id, template_i
     template = oauth_ac.request('GET', request_url)
 
     for item in template:
-        for ig in (filter(lambda x: x['id']==ig_id, item['indicatorGroups'])):
+        for ig in (filter(lambda x: x['id'] == ig_id, item['indicatorGroups'])):
             group_name = ig['internalId']
 
             for indicator in (filter(lambda x: x['internalId'] not in uploaded_indicators, ig['indicators'])):
                 missing.append(indicator['internalId'])
     if missing:
-        raise RuntimeError(f'Indicators {missing} in indicator group {group_name} are not in dataset. '\
-                            'Update would overwrite missing indicators with "NaN" for the time period. '\
+        raise RuntimeError(f'Indicators {missing} in indicator group {group_name} are not in dataset. ' +
+                            'Update would overwrite missing indicators with "NaN" for the time period. ' +
                             'If this is wanted, use "force_update" in the function call.')
 
 
-def upload_indicator_data(dataset: TimeseriesDataset, force_update = ''):
+def upload_indicator_data(dataset: TimeseriesDataset, force_update=''):
     """
     Upload a `TimeseriesDataset` to SAP IoT.
 
@@ -104,15 +104,18 @@ def upload_indicator_data(dataset: TimeseriesDataset, force_update = ''):
     dataset
         TimeseriesDataset of indicators to be updated to SAP IoT.
     force_update
-        A flag to force an update of an IndicatorGroup with some indicators. Indicators which are not in dataset will be set to 'NaN' for period of time
+        A flag to force an update of an IndicatorGroup with some indicators. 
+        Indicators which are not in dataset will be set to 'NaN' for period of time
 
     Examples
     --------
-    Force update timeseries data of IndicatorGroup 'my_indicator_group'. Dataset 'my_some_timeseries_data' includes only some indicators of 'my_indicator_group'::
+    Force update timeseries data of IndicatorGroup 'my_indicator_group'. 
+    Dataset 'my_some_timeseries_data' includes only some indicators of 'my_indicator_group'::
 
         upload_indicator_data(my_some_timeseries_data, force_update = 'x')
 
-    Update timeseries data of 'my_indicator_group' indicators. Dataset 'my_timeseries_data' has data of all indicators in the group::
+    Update timeseries data of 'my_indicator_group' indicators. 
+    Dataset 'my_timeseries_data' has data of all indicators in the group::
 
         upload_indicator_data(my_timeseries_data)
     """
@@ -131,4 +134,3 @@ def upload_indicator_data(dataset: TimeseriesDataset, force_update = ''):
             _check_indicator_group_is_complete(uploaded_indicators, group_id, template_id)
 
         _upload_data_single_indicator_group(dataset, selected_indicator_set, group_id, template_id)
-
