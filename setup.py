d2257e1f35d5b6d6bf36343b8ddf121df368a6c5
Oliver Röss <oliver.roess@sap.com>
2022-04-21 11:29:22 +0200

    Officially support Python 3.10 (#111)
    * Add Python 3.10 to test matrix and supported versions
    * Update package dependencies (sailor, docs)
    * unrelated: use latest python 3.8 version in Jenkins

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
     project_urls={  # Optional
         'Documentation': 'https://sap.github.io/project-sailor',
